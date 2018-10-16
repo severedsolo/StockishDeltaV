cb8a70b5b59b2f4cba556b71a3d67a1ea36896ec
severedsolo <severedsolo@users.noreply.github.com>
2018-10-16 21:46:31 +0100

    Initial commit

---
+++ DeltaVTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using PreFlightTests;
+
+namespace StockishDeltaV
+{
+    class DeltaVTest : DesignConcernBase
+    {           
+        public override string GetConcernDescription()
+        {
+            if (EditorLogic.fetch.ship.vesselDeltaV == null) return "";
+            return "Atmosphere: " + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVASL,0) + "m/s"+Environment.NewLine+ "Vacuum DeltaV:" + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVVac,0)+"m/s";
+        }
+
+        public override string GetConcernTitle()
+        {
+            if (EditorLogic.fetch.ship.vesselDeltaV == null) return "Delta V: Not Ready";
+            return "Delta V: " + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVVac,0)+"m/s";
+        }
+
+        public override DesignConcernSeverity GetSeverity()
+        {
+            return DesignConcernSeverity.NOTICE;
+        }
+
+        public override bool TestCondition()
+        {
+            bool condition = EditorLogic.fetch.ship.vesselDeltaV == null;
+            Debug.Log("[StockishDeltaV] condition = " + condition);
+            return condition;
+        }
+    }
+}
---
+++ StockishDeltaV.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KSP.UI.Screens;
+using UnityEngine;
+using PreFlightTests;
+
+namespace StockishDeltaV
+{
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    public class StockishDeltaV : MonoBehaviour
+    {
+        public static StockishDeltaV instance;
+        DeltaVTest test;
+
+        void Awake()
+        {
+            if (instance != null) Destroy(this);
+            else instance = this;
+            GameEvents.onGUIEngineersReportReady.Add(ReportReady);
+            GameEvents.onGUIEngineersReportDestroy.Add(ReportDestroyed);
+        }
+
+        private void AddTest()
+        {
+            if(EditorLogic.fetch.ship.vesselDeltaV == null)
+            {
+                Invoke("AddTest", 0.5f);
+                return;
+            }
+            if(!EditorLogic.fetch.ship.vesselDeltaV.isReady)
+            {
+                Invoke("AddTest", 0.5f);
+                return;
+            }
+            test = new DeltaVTest();
+            EngineersReport.Instance.AddTest(test);
+        }
+        private void ReportDestroyed()
+        {
+            if(test != null) EngineersReport.Instance.RemoveTest(test);
+        }
+
+        private void ReportReady()
+        {
+            Debug.Log("[StockishDeltaV] ReportReady");
+            if (test != null) EngineersReport.Instance.RemoveTest(test);
+            AddTest();
+        }
+
+    }
+}
