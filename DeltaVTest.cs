2f4f0a6a6b5bb780d879e979f620aa3918926bbe
Thygrrr <github@thygrrr.de>
2018-10-17 02:34:15 +0200

    Coroutine changes and some comments added where bugs are, but I could not find a solution

--- DeltaVTest.cs
+++ DeltaVTest.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 using PreFlightTests;
 
@@ -11,14 +8,24 @@ namespace StockishDeltaV
     {           
         public override string GetConcernDescription()
         {
-            if (EditorLogic.fetch.ship.vesselDeltaV == null) return "";
-            return "Atmosphere: " + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVASL,0) + "m/s"+Environment.NewLine+ "Vacuum DeltaV:" + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVVac,0)+"m/s";
+            //FIXME: This has the problem that when the ship gets changed, the sim needs some ticks to run again.
+            //Maybe start some kind of delayed info in GameEvents.onEditorPartPlaced
+            if (EditorLogic.fetch.ship.vesselDeltaV == null || !EditorLogic.fetch.ship.vesselDeltaV.isReady)
+            {
+                return "DeltaV not ready"; //FIXME: This never occurs?
+            }
+            return "Atmosphere: " + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVASL, 0) + "m/s" + Environment.NewLine + "Vacuum DeltaV:" + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVVac, 0) + "m/s";
         }
 
         public override string GetConcernTitle()
         {
-            if (EditorLogic.fetch.ship.vesselDeltaV == null) return "Delta V: Not Ready";
-            return "Delta V: " + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVVac,0)+"m/s";
+            //FIXME: This has the problem that when the ship gets changed, the sim needs some ticks to run again.
+            //Maybe start some kind of delayed info in GameEvents.onEditorPartPlaced
+            if (EditorLogic.fetch.ship.vesselDeltaV == null || !EditorLogic.fetch.ship.vesselDeltaV.isReady)
+            {
+                return "DeltaV not ready";  //FIXME: This never occurs?
+            }
+            return "Delta V: " + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVVac, 0) + "m/s";
         }
 
         public override DesignConcernSeverity GetSeverity()
@@ -28,9 +35,7 @@ namespace StockishDeltaV
 
         public override bool TestCondition()
         {
-            bool condition = EditorLogic.fetch.ship.vesselDeltaV == null;
-            Debug.Log("[StockishDeltaV] condition = " + condition);
-            return condition;
+            return false;
         }
     }
 }
