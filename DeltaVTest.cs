e183e14c5b2823dc04487514813b2747e9774015
severedsolo <severedsolo@users.noreply.github.com>
2018-10-17 07:26:12 +0100

    Clearer notes

--- DeltaVTest.cs
+++ DeltaVTest.cs
@@ -15,7 +15,7 @@ namespace StockishDeltaV
         {
             //FIXME: This has the problem that when the ship gets changed, the sim needs some ticks to run again.
             //Maybe start some kind of delayed info in GameEvents.onEditorPartPlaced
-            //Yielding until the sim is ready should do the trick, but the report still doesn't update. I suspect the report only builds itself when it's drawn like PopUpDialog does.
+            //Waiting until the sim is ready should do the trick, but the report still doesn't update. I suspect the report only builds itself when it's drawn like PopUpDialog does.
             //Perhaps find a way to force the window to close/reopen itself when the sim is done?
             return "Atmosphere: " + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVASL, 0) + "m/s" + Environment.NewLine + "Vacuum:" + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVVac, 0) + "m/s";
         }
@@ -24,7 +24,7 @@ namespace StockishDeltaV
         {
             //FIXME: This has the problem that when the ship gets changed, the sim needs some ticks to run again.
             //Maybe start some kind of delayed info in GameEvents.onEditorPartPlaced
-            //Yielding until the sim is ready should do the trick, but the report still doesn't update. I suspect the report only builds itself when it's drawn like PopUpDialog does.
+            //Waiting until the sim is ready should do the trick, but the report still doesn't update. I suspect the report only builds itself when it's drawn like PopUpDialog does.
             //Perhaps find a way to force the window to close/reopen itself when the sim is done?
             return "Delta V: " + Math.Round(EditorLogic.fetch.ship.vesselDeltaV.totalDeltaVVac, 0) + "m/s";
         }
